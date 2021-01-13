4506404669f998e5d6ec8147391d2076622af078
leshalotos <leshadiplom@mail.ru>
2021-01-13 14:55:26 +0300

    AllAdd

--- Vectors/src/com/Vectors/Main.java
+++ Vectors/src/com/Vectors/Main.java
@@ -1,8 +1,19 @@
 package com.Vectors;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Vector result = new Vector();
+
+        System.out.println("Haw many vectors do you want add?");
+
+        Vector[] array = OperationsVector.setArrayVectors(OperationsVector.setIntegerValue());
+
+        result = OperationsVector.addArrayVectors(array);
+
+        System.out.println(result.toString());
     }
 }
